ce36762198c413631b783223d4a860dd55133b1d
juliojala <131935736+juliojala@users.noreply.github.com>
2023-05-05 19:19:24 -0300

    Revert "jala"
    This reverts commit ef6a3950be9f0ab46ebfed17f40169b3936a4685.

--- dino_runner/utils/text_utils.py
+++ dino_runner/utils/text_utils.py
@@ -2,8 +2,8 @@ import pygame
 
 from dino_runner.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
-FONT_COLOR = (255, 255, 255)
-FONT_SIZE = 30
+FONT_COLOR = (0, 0, 0)
+FONT_SIZE = 22
 FONT_STYLE = "freesansbold.ttf"
 
 
