ef6a3950be9f0ab46ebfed17f40169b3936a4685
juliojala <131935736+juliojala@users.noreply.github.com>
2023-05-05 19:07:52 -0300

    jala
    projeto final

--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -1,73 +1,110 @@
 import pygame
 import os
+pygame.mixer.init()
 
 # Global Constants
-TITLE = "Chrome Dino Runner"
+TITLE = "Sonic Runner"
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 FPS = 30
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
+JUMP_SOUND = pygame.mixer.Sound(os.path.join(IMG_DIR, "Sounds/Jump.wav"))
+DUCK_SOUND = pygame.mixer.Sound(os.path.join(IMG_DIR, "Sounds/DropDash.wav"))
+RING_SOUND = pygame.mixer.Sound(os.path.join(IMG_DIR, "Sounds/Ring.wav"))
+DESTROY = pygame.mixer.Sound(os.path.join(IMG_DIR, "Sounds/Destroy.wav"))
+DEATH_SOUND = pygame.mixer.Sound(os.path.join(IMG_DIR, "Sounds/Hurt.wav"))
+
+MENU_BLEEP = pygame.mixer.Sound(os.path.join(IMG_DIR, "Sounds/MenuBleep.wav"))
+
+BG_SOUND = pygame.mixer.music.load(os.path.join(IMG_DIR, "Sounds/GHZAct1.mp3"))
+
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
+ICON = pygame.image.load(os.path.join(IMG_DIR, "ICON.png"))
+icon_death = pygame.image.load(os.path.join(IMG_DIR, "Other/DEATH.png"))
+ICON_DEATH = pygame.transform.scale(icon_death, (45 * 3, 45 * 3))
+start_menu = pygame.image.load(os.path.join(IMG_DIR, "Other/Telainicial.png"))
+START_MENU = pygame.transform.scale(start_menu, (250 * 1.2, 250 * 1.2))
 
-RUNNING = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
-]
+RUN = pygame.image.load(os.path.join(IMG_DIR, "Dino/RUN.png"))
 
-RUNNING_SHIELD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1Shield.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
-]
+RUNNING = []
+for i in range(8):
+    img_run = RUN.subsurface((i * 45, 0), (45, 45))
+    img_run = pygame.transform.scale(img_run, (45 * 2, 45 * 2))
+    RUNNING.append(img_run)
 
-RUNNING_HAMMER = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
-]
+RUN_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/RUN-SHIELD.png"))
 
-JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJump.png"))
-JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpShield.png"))
-JUMPING_HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpHammer.png"))
+RUNNING_SHIELD = []
+for i in range(8):
+    img_run_shield = RUN_SHIELD.subsurface((i * 45, 0), (45, 45))
+    img_run_shield = pygame.transform.scale(img_run_shield, (45 * 2, 45 * 2))
+    RUNNING_SHIELD.append(img_run_shield)
 
-DUCKING = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
-]
+JUMP = pygame.image.load(os.path.join(IMG_DIR, "Dino/JUMP.png"))
+JUMPING = []
+for i in range(16):
+    img_jump = JUMP.subsurface((i * 45, 0), (45, 45))
+    img_jump = pygame.transform.scale(img_jump, (45 * 2, 45 * 2))
+    JUMPING.append(img_jump)
 
-DUCKING_SHIELD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Shield.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
-]
+jump_shield = pygame.image.load(os.path.join(IMG_DIR, "Dino/JUMP-SHIELD.png"))
+JUMPING_SHIELD = []
+for i in range(16):
+    img_jump_shield = jump_shield.subsurface((i * 45, 0), (45, 45))
+    img_jump_shield = pygame.transform.scale(img_jump_shield, (45 * 2, 45 * 2))
+    JUMPING_SHIELD.append(img_jump_shield)
 
-DUCKING_HAMMER = [
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
-]
+duck = pygame.image.load(os.path.join(IMG_DIR, "Dino/BEND-OVER.png"))
+DUCKING = []
+for i in range(16):
+    img_duck = duck.subsurface((i * 45, 0), (45, 45))
+    img_duck = pygame.transform.scale(img_duck, (45 * 2, 45 * 2))
+    DUCKING.append(img_duck)
 
-SMALL_CACTUS = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
-]
-LARGE_CACTUS = [
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus3.png")),
-]
 
-BIRD = [
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1.png")),
-    pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2.png")),
-]
+duck_shield = pygame.image.load(os.path.join(IMG_DIR, "Dino/BEND-OVER-SHIELD.png"))
+DUCKING_SHIELD = []
+for i in range(16):
+    img_duck_shield = duck_shield.subsurface((i * 45, 0), (45, 45))
+    img_duck_shield = pygame.transform.scale(img_duck_shield, (45 * 2, 45 * 2))
+    DUCKING_SHIELD.append(img_duck_shield)
 
-CLOUD = pygame.image.load(os.path.join(IMG_DIR, 'Other/Cloud.png'))
-SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
-HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png'))
+small_obstacle = pygame.image.load(os.path.join(IMG_DIR, "Cactus/SMALL-OBSTACLE.png"))
+SMALL_CACTUS = []
+for i in range(3):
+    img_small = small_obstacle.subsurface((i * 25, 0), (25, 25))
+    img_small = pygame.transform.scale(img_small, (25 * 3, 25 * 3))
+    SMALL_CACTUS.append(img_small)
 
-BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
+large_obstacle = pygame.image.load(os.path.join(IMG_DIR, "Cactus/LARGE-OBSTACLE.png"))
+LARGE_CACTUS = []
+for i in range(3):
+    img_large = large_obstacle.subsurface((i * 30, 0), (30, 30))
+    img_large = pygame.transform.scale(img_large, (30 * 3, 30 * 3))
+    LARGE_CACTUS.append(img_large)
 
-HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
+bird = pygame.image.load(os.path.join(IMG_DIR, "Bird/ENEMY.png"))
+
+BIRD = []
+for i in range(9):
+    img_run = bird.subsurface((i * 45, 0), (45, 45))
+    img_run = pygame.transform.scale(img_run, (45 * 2, 45 * 2))
+    BIRD.append(img_run)
+
+
+cloud = pygame.image.load(os.path.join(IMG_DIR, 'Other/NUVEM.png'))
+CLOUD = pygame.transform.scale(cloud, (45 * 2, 45 * 2))
+
+shield = pygame.image.load(os.path.join(IMG_DIR, 'Other/HYPER-RING.png'))
+SHIELD = []
+for i in range(16):
+    img_run = shield.subsurface((i * 45, 0), (45, 45))
+    img_run = pygame.transform.scale(img_run, (45 * 2, 45 * 2))
+    SHIELD.append(img_run)
+
+BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/FLOOR.png'))
 
 DEFAULT_TYPE = "default"
 SHIELD_TYPE = "shield"
